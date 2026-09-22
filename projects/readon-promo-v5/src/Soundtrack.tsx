37a2aff9ed23d44af78ac76faa394cef02335edf
yutateru6-collab <yutateru6@gmail.com>
2026-09-22 19:19:51 +0900

    fix: align V5 voice and SFX timing

--- projects/readon-promo-v5/src/Soundtrack.tsx
+++ projects/readon-promo-v5/src/Soundtrack.tsx
@@ -3,15 +3,15 @@ import {Audio} from '@remotion/media';
 import {Sequence, staticFile} from 'remotion';
 
 const voices = [
-  {from: 0, duration: 45, file: 'vo-hook.mp3'},
-  {from: 45, duration: 45, file: 'vo-quiz.mp3'},
-  {from: 90, duration: 36, file: 'vo-answer.mp3'},
-  {from: 126, duration: 54, file: 'vo-interest.mp3'},
-  {from: 180, duration: 60, file: 'vo-topic.mp3'},
-  {from: 240, duration: 45, file: 'vo-read.mp3'},
-  {from: 285, duration: 45, file: 'vo-translate.mp3'},
-  {from: 330, duration: 45, file: 'vo-explain.mp3'},
-  {from: 375, duration: 75, file: 'vo-close.mp3'},
+  {from: 0, duration: 48, file: 'vo-hook.mp3'},
+  {from: 48, duration: 36, file: 'vo-quiz.mp3'},
+  {from: 84, duration: 48, file: 'vo-answer.mp3'},
+  {from: 132, duration: 60, file: 'vo-interest.mp3'},
+  {from: 192, duration: 60, file: 'vo-topic.mp3'},
+  {from: 252, duration: 30, file: 'vo-read.mp3'},
+  {from: 282, duration: 48, file: 'vo-translate.mp3'},
+  {from: 330, duration: 54, file: 'vo-explain.mp3'},
+  {from: 384, duration: 84, file: 'vo-close.mp3'},
 ] as const;
 
 export const Soundtrack: React.FC = () => (
@@ -28,25 +28,25 @@ export const Soundtrack: React.FC = () => (
       </Sequence>
     ))}
 
-    {[44, 89, 125, 179, 239, 284, 329, 374].map((at) => (
+    {[47, 83, 131, 191, 251, 281, 329, 383].map((at) => (
       <Sequence key={'whoosh-' + at} from={at}>
         <Audio src={staticFile('audio/whoosh.wav')} volume={0.22} />
       </Sequence>
     ))}
 
-    {[16, 63, 82, 101, 204, 260, 305, 350].map((at) => (
+    {[16, 62, 80, 101, 216, 265, 305, 350].map((at) => (
       <Sequence key={'click-' + at} from={at}>
         <Audio src={staticFile('audio/click.wav')} volume={0.22} />
       </Sequence>
     ))}
 
-    <Sequence from={90}>
+    <Sequence from={84}>
       <Audio src={staticFile('audio/impact.wav')} volume={0.40} />
     </Sequence>
-    <Sequence from={374}>
+    <Sequence from={383}>
       <Audio src={staticFile('audio/riser.wav')} volume={0.24} />
     </Sequence>
-    <Sequence from={375}>
+    <Sequence from={384}>
       <Audio src={staticFile('audio/impact.wav')} volume={0.42} />
     </Sequence>
   </>
